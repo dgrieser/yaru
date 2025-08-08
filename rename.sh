f462dea51d53bcf12d61ff70c854b2ab9048a730
David Grieser <d.grieser@mittwald.de>
2025-08-08 15:13:53 +0200

    Add files and content.

--- rename.sh
+++ rename.sh
@@ -2,10 +2,16 @@
 
 echo "Renaming folders..." 1>&2
 find . -type d -iname "*yaru*" | grep -v "^./.git" | while IFS= read -r dir; do
-    mv -v "$dir" "$(echo "$dir" | sed -E 's/(yaru)/\1dave/ig')"
+    mv -v "${dir}" "$(echo "${dir}" | sed -E 's/(yaru)/\1dave/ig')"
 done
 
 echo "Renaming files..." 1>&2
 find . -type f -iname "*yaru*" | grep -v "^./.git" | while IFS= read -r file; do
-    mv -v "$file" "$(echo "$file" | sed -E 's/(yaru)/\1dave/ig')"
+    mv -v "${file}" "$(echo "${file}" | sed -E 's/(yaru)/\1dave/ig')"
+done
+
+echo "Renaming in content..." 1>&2
+grep -lIir "yaru" . | grep -v "^./.git" | while IFS= read -r file; do
+    echo "Replacing content in ${file}..."
+    sed -i -E 's/(yaru)/\1dave/ig' "${file}"
 done
