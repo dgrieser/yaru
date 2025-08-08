68c5fcf0b4634566826305918c10cf80ed646c4f
David Grieser <d.grieser@mittwald.de>
2025-08-08 15:19:33 +0200

    Fix

--- rename.sh
+++ rename.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+APP="$(basename "${0}")"
+
 echo "Renaming folders..." 1>&2
 find . -type d -iname "*yaru*" | grep -v "^./.git" | while IFS= read -r dir; do
     mv -v "${dir}" "$(echo "${dir}" | sed -E 's/(yaru)/\1dave/ig')"
@@ -11,7 +13,7 @@ find . -type f -iname "*yaru*" | grep -v "^./.git" | while IFS= read -r file; do
 done
 
 echo "Renaming in content..." 1>&2
-grep -lIir "yaru" . | grep -v "^./.git" | while IFS= read -r file; do
+grep -lIir "yaru" . | grep -v "^./.git" | grep -v "^./${APP}$" | while IFS= read -r file; do
     echo "Replacing content in ${file}..."
     sed -i -E 's/(yaru)/\1dave/ig' "${file}"
     sed -i -E 's/(yaru)dave\.git/\1\.git/ig' "${file}"
